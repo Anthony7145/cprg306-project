81425203efb82d5ddc51e6657ee12afd22c54484
Idemili Ifechukwu Anthony <119919902+Anthony7145@users.noreply.github.com>
2023-12-08 15:53:45 -0700

    Add files via upload

--- app/Home/Survey.js
+++ app/Home/Survey.js
@@ -5,8 +5,41 @@ const Survey = () => {
   return (
     <div>
       <h1>Survey Page</h1>
-      {/* Add your survey form or content here */}
+      <p>Please answer the following questions:</p>
+      <form>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input type="email" name="email" />
+        </label>
+        <br />
+        <label>
+          Age:
+          <input type="number" name="age" />
+        </label>
+        <br />
+        <label>
+          Gender:
+          <select name="gender">
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
+        </label>
+        <br />
+        <label>
+          How did you hear about us?
+          <input type="text" name="referral" />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
     </div>
+
   );
 };
 
