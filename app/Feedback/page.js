e5e3f4ecf5f8bf5913fe4b8eeea075080bb07dfb
Idemili Ifechukwu Anthony <119919902+Anthony7145@users.noreply.github.com>
2023-12-08 15:51:48 -0700

    Add files via upload

--- app/Feedback/page.js
+++ app/Feedback/page.js
@@ -1,7 +1,26 @@
-
-import React from 'react';
+import React, { useState } from 'react';
+import { useClient } from 'next/client';
 
 export default function Feedback(){
+  const [submitted, setSubmitted] = useState(false);
+  const [rating, setRating] = useState(0);
+  const [feedback, setFeedback] = useState('');
+
+  const handleRatingChange = (value) => {
+    setRating(value);
+  };
+  
+  const handleFeedbackChange = (e) => {
+    setFeedback(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ rating, feedback });
+    setSubmitted(true);
+  };
+
+  const client = useClient();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -19,9 +38,51 @@ export default function Feedback(){
         </ol>
       </div>
       
+      <div>
+      {submitted ? (
+        <div>
+          <h2>Thank you for your feedback!</h2>
+          <p>We appreciate your time and input.</p>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <h3>Feedback Form</h3>
+          <div>
+            <label>Rate us:</label>
+            <div>
+              {[1, 2, 3, 4, 5].map((value) => (
+                <label key={value}>
+                  <input
+                    type="radio"
+                    name="rating"
+                    value={value}
+                    onChange={() => handleRatingChange(value)}
+                  />
+                  {value}
+                </label>
+              ))}
+            </div>
+          </div>
+          <div>
+            <label>Your Feedback:</label>
+            <textarea
+              value={feedback}
+              onChange={handleFeedbackChange}
+              rows="4"
+              cols="50"
+              required
+            />
+          </div>
+          <button type="submit">Submit Feedback</button>
+        </form>
+      )}
+    </div>
     </div>
     </main>
+
+
   );
 };
+
 
 
